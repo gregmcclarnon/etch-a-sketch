0ba95dadf8fc6a3bc4fbb745d47e3911c2b1842e
Greg McClarnon <greg@splaatphoto.co.uk>
2024-02-26 17:40:20 +0000

    Feat: user can choose grid size

--- javascript.js
+++ javascript.js
@@ -1,21 +1,45 @@
-function createGrid() {
+function createGrid(sides) {
 
     let flexContainer = document.getElementById('container');
 
-    for (let i = 0; i < 256; i++) {
-        flexContainer.innerHTML += `<div class="grid-item"></div>`;
+    flexContainer.innerHTML = '';
+
+    let sideSize = 640 / sides;
+
+    sideSize += "px"
+
+    for (let i = 0; i < sides*sides; i++) {
+        flexContainer.innerHTML += `<div class="grid-item" style="width: ${sideSize}; height: ${sideSize}; border: 1px solid #000;"></div>`;
     }
 
 }
 
-createGrid();
-
-// Listens for mouse over and adds class .touch to the grid item
-
-document.querySelectorAll('.grid-item').forEach(function(item) {
-    item.addEventListener('mouseenter', function() {
-        this.classList.add('touch');
+function addMouseOverListeners() {
+    // Listens for mouse over and adds class .touch to the grid item
+    document.querySelectorAll('.grid-item').forEach(function(item) {
+        item.addEventListener('mouseenter', function() {
+            this.classList.add('touch');
+        });
     });
-});
+    
+    }
 
 
+
+createGrid(16);
+addMouseOverListeners();
+
+
+
+
+
+function reDrawGrid() {
+    sides = prompt("How many squares would you like on each side?")
+
+    createGrid(sides);
+    addMouseOverListeners();
+
+
+
+
+}
