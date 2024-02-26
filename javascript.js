15cf86680f432475c3a5c37612957e0f3a141be6
Greg McClarnon <greg@splaatphoto.co.uk>
2024-02-26 17:18:57 +0000

    Feat: On mouse over the grid item turns red

--- javascript.js
+++ javascript.js
@@ -3,15 +3,19 @@ function createGrid() {
     let flexContainer = document.getElementById('container');
 
     for (let i = 0; i < 256; i++) {
-
-        flexContainer.innerHTML += `<div class ="grid-item"></div>`;
-
-
+        flexContainer.innerHTML += `<div class="grid-item"></div>`;
     }
-
-    
 
 }
 
-
 createGrid();
+
+// Listens for mouse over and adds class .touch to the grid item
+
+document.querySelectorAll('.grid-item').forEach(function(item) {
+    item.addEventListener('mouseenter', function() {
+        this.classList.add('touch');
+    });
+});
+
+
